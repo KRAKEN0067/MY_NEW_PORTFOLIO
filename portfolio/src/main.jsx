bc4f74cda75306e221b8aeb16c922f19c9d18047
aryan baakle <aryanbaakle@gmail.com>
2025-08-09 15:42:30 +0530

    fixed errors'

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "core-js/modules/es.promise";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
