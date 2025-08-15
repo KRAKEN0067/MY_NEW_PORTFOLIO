888cdac6d16828e8bb3de022cd1739f30c2a2684
aryan baakle <aryanbaakle@gmail.com>
2025-08-15 22:43:14 +0530

    changed name font

--- portfolio/src/components/home/Name.jsx
+++ portfolio/src/components/home/Name.jsx
@@ -5,8 +5,8 @@ const Name = () => {
   return (
     <RevealOnScroll>
       <div className="flex justify-center items-center pt-64">
-        <h1 className="animate-text-gradient text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-green-600 text-shadow-glow">
-          ARYAN BAAKLE
+        <h1 className="animate-text-gradient text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-green-600 text-shadow-glow font-serif italic pb-10">
+          Aryan Baakle
         </h1>
       </div>
     </RevealOnScroll>
