339f7ad14388e1383e660477a1bcd7245bea9933
aryan baakle <aryanbaakle@gmail.com>
2025-08-21 21:48:37 +0530

    hover effect in skill card

--- portfolio/src/components/Skills/Skillcard.jsx
+++ portfolio/src/components/Skills/Skillcard.jsx
@@ -3,7 +3,7 @@ import RevealOnScroll from "../global/RevealOnScroll";
 const Skillcard = ({ skill }) => {
   return (
     <RevealOnScroll>
-      <div className="flex flex-col items-center justify-center p-4 border-2 border-green-600 gap-2 rounded-xl hover:scale-110 hover:shadow-xl hover:bg-green-950/20 transition-all duration-300">
+      <div className="group relativeflex flex-col items-center justify-center p-4 border-2 border-green-600 gap-2 rounded-xl hover:scale-110 hover:shadow-xl hover:bg-green-950/70 transition-all duration-300">
         <img
           height={60}
           width={60}
@@ -11,7 +11,10 @@ const Skillcard = ({ skill }) => {
           alt={skill.alt}
           className="object-contain mb-2 p-2"
         />
-        {/* <p className="overflow-hidden text-gray-200 text-sm font-medium">{skill.alt}</p> */}
+        <div className="flex justify-between w-full">
+          <p className="text-white text-sm font-bold opacity-0 group-hover:opacity-100 transition-all duration-300">{skill.alt}</p>
+          <p className="text-white text-xs font-bold opacity-0 group-hover:opacity-100 transition-all duration-300">{skill.level}%</p>
+        </div>
         <div className="w-full bg-gray-800 rounded-xs h-2 overflow-hidden">
           <div
             className="bg-green-600 h-4 text-xs rounded-xs font-bold flex items-center justify-center text-white transition-all duration-700 ease-in-out"
